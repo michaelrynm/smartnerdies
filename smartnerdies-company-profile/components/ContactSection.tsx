aa8225833aa3e6ebe1ed53c8b27dddb43f35d21c
michaelynm <michaelmarcelino55@gmail.com>
2025-07-13 10:21:34 +0700

    resolve build error

--- smartnerdies-company-profile/components/ContactSection.tsx
+++ smartnerdies-company-profile/components/ContactSection.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { useState } from "react";
 import { FaInstagram, FaTiktok } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
